6a96238f2c23eb00b782b73a648ccdcf647df9e8
Chris <christoph@latocha.de>
2018-12-15 16:56:00 +0100

    bug fixes

--- Aufgabe09/Program.cs
+++ Aufgabe09/Program.cs
@@ -30,7 +30,7 @@ namespace Aufgabe09
             Console.WriteLine("Willkommen");
             do
             {
-                
+
                 Console.WriteLine("Punkte: " + Points);
                 Console.WriteLine("Beantwortete Fragen: " + attemptedQuestions);
                 Console.WriteLine("Welche Option:");
@@ -98,61 +98,19 @@ namespace Aufgabe09
             switch (Type)
             {
                 case "1":
-                    Console.WriteLine("Bitte Richtige Antwort eingeben!");
-                    String answertext = Console.ReadLine();
-                    questionCatalogue.Add(new WritingQ(Questiontext, answertext));
+                    questionCatalogue.Add(AddWritingQuestion(Questiontext));
                     break;
                 case "2":
-                    Console.WriteLine("Schreib y wenns richtig ist ein und n wenn nicht.");
-                    string input = Console.ReadLine();
-                    bool isCorrectYoN = false;
-                    if (input == "y")
-                    {
-                        isCorrectYoN = true;
-                    }
-                    questionCatalogue.Add(new YesNoQ(Questiontext, isCorrectYoN));
+                    questionCatalogue.Add(AddYesNoQuestion(Questiontext));
                     break;
                 case "3":
-                    Console.WriteLine("Bitte die Richtige Zahl eingeben.");
-                    int numberToGuess = Int32.Parse(Console.ReadLine());
-                    questionCatalogue.Add(new EstimationQuestion(Questiontext, numberToGuess));
+                    questionCatalogue.Add(AddEstimationQuestion(Questiontext));
                     break;
                 case "4":
-                    Console.WriteLine("Wieviele Antworten soll es geben?");
-                    int answerCount = Int32.Parse(Console.ReadLine());
-                    List<Answer> newAnswer = new List<Answer>();
-                    Answer userAnswer = new Answer();
-                    bool isCorrect;
-                    string text;
-                    for (int i = 0; i < answerCount; i++)
-                    {
-                        Console.WriteLine("Bitte Anwort eingeben");
-                        text = Console.ReadLine();
-                        Console.WriteLine("Schreib y wenns richtig ist ein und n wenn nicht.");
-                        if (Console.ReadLine() == "y")
-                        {
-                            isCorrect = true;
-                        }
-                        else
-                        {
-                            isCorrect = false;
-                        }
-                        newAnswer.Add(new Answer(text, isCorrect));
-                    }
-                    questionCatalogue.Add(new MultipleAnswers(Questiontext, newAnswer));
+                    questionCatalogue.Add(AddMultipleAnswersQuestion(Questiontext));
                     break;
                 case "5":
-                    Console.WriteLine("Wieviele Antworten soll es geben?");
-                    int answerCountSingle = Int32.Parse(Console.ReadLine());
-                    List<Answer> newAnswerS = new List<Answer>();
-                    Console.WriteLine("Bitte Richtige Antwtort eingeben!");
-                    newAnswerS.Add(new Answer(Console.ReadLine(), true));
-                    for (int i = 0; i < answerCountSingle; i++)
-                    {
-                        Console.WriteLine("Bitte Anwort eingeben");
-                        newAnswerS.Add(new Answer(Console.ReadLine(), false));
-                    }
-                    questionCatalogue.Add(new MultipleChoiceQ(Questiontext, newAnswerS));
+                    questionCatalogue.Add(AddMultipleChoiceQuestion(Questiontext));
                     break;
                 default:
                     break;
@@ -160,6 +118,72 @@ namespace Aufgabe09
             WriteQuizCatalogueToFile(questionCatalogue);
 
         }
+        public static Question AddWritingQuestion(string Question)
+        {
+            Console.WriteLine("Bitte Richtige Antwort eingeben!");
+            String answerFree = Console.ReadLine();
+            return new WritingQ(Question, answerFree);
+        }
+
+        public static Question AddYesNoQuestion(string question)
+        {
+            Console.WriteLine("Schreib y wenns richtig ist ein und n wenn nicht.");
+            string input = Console.ReadLine();
+            bool isCorrectYoN = false;
+            if (input == "y")
+            {
+                isCorrectYoN = true;
+            }
+            return new YesNoQ(question, isCorrectYoN);
+        }
+
+        public static Question AddEstimationQuestion(string question)
+        {
+            Console.WriteLine("Bitte die Richtige Zahl eingeben.");
+            int numberToGuess = Int32.Parse(Console.ReadLine());
+            return new EstimationQuestion(question, numberToGuess);
+        }
+
+        public static Question AddMultipleAnswersQuestion(string question)
+        {
+            Console.WriteLine("Wieviele Antworten soll es geben?");
+            int answerCount = Int32.Parse(Console.ReadLine());
+            List<Answer> newAnswer = new List<Answer>();
+            Answer userAnswer = new Answer();
+            bool isCorrect;
+            string text;
+            for (int i = 0; i < answerCount; i++)
+            {
+                Console.WriteLine("Bitte Anwort eingeben");
+                text = Console.ReadLine();
+                Console.WriteLine("Schreib y wenns richtig ist ein und n wenn nicht.");
+                if (Console.ReadLine() == "y")
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    isCorrect = false;
+                }
+                newAnswer.Add(new Answer(text, isCorrect));
+            }
+            return new MultipleAnswers(question, newAnswer);
+        }
+        public static Question AddMultipleChoiceQuestion(string question)
+        {
+            Console.WriteLine("Wieviele Antworten soll es geben?");
+            int answerCountSingle = Int32.Parse(Console.ReadLine());
+            List<Answer> newAnswerS = new List<Answer>();
+            Console.WriteLine("Bitte Richtige Antwtort eingeben!");
+            newAnswerS.Add(new Answer(Console.ReadLine(), true));
+            for (int i = 0; i < answerCountSingle; i++)
+            {
+                Console.WriteLine("Bitte Anwort eingeben");
+                newAnswerS.Add(new Answer(Console.ReadLine(), false));
+            }
+            return new MultipleChoiceQ(question, newAnswerS);
+        }
+
         static JsonSerializerSettings settings = new JsonSerializerSettings
         {
             TypeNameHandling = TypeNameHandling.Objects,
