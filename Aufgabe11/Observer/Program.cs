3480bc2aeb532b02b5c11bc4d5b4187a966cea57
Chris <christoph@latocha.de>
2019-01-08 16:50:21 +0100

    to dos

---
+++ Aufgabe11/Observer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Calculator C1 = new Calculator();
+            Calculator C2 = new Calculator();
+            C1.CalculateSomething();
+            C2.CalculateSomething();
+
+        }
+    }
+
+
+    public delegate void ReportProgressMethod(int progress);
+    public class Calculator
+    {
+        int k = 10;
+
+        public event ReportProgressMethod ProgressMethod = ProgressReport;
+        
+
+
+        public static void ProgressReport(int progress)
+        {
+            Console.WriteLine(progress);
+        }
+  
+
+        public void CalculateSomething()
+        {
+
+            for (int i = 0; i <= 8; i++)
+            {
+                if (i == 4)
+                {
+                    ProgressMethod(50);
+                }
+
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (j == 5)
+                    {
+                        ProgressMethod(k);
+                        k += 10;
+                    }
+
+                }
+
+            }
+            ProgressMethod(100);
+        }
+
+    }
+}
---
+++ Aufgabe11/Singleton/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id = IDGenerator.Instance.GenerateNewID();
+            Console.WriteLine(Id);
+            int Id2 = IDGenerator.Instance.GenerateNewID();
+            Console.WriteLine(Id2);
+        }
+    }
+
+
+    public class IDGenerator
+    {
+        private int ID;
+        private IDGenerator()
+        {
+            ID = 1;
+        }
+
+        private static IDGenerator _instance;
+
+        public static IDGenerator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new IDGenerator();
+                return _instance;
+            }
+        }
+        public int GenerateNewID()
+        {
+            return ID++;
+        }
+
+    }
+
+
+}
